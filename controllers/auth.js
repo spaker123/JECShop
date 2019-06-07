29fac897d8b3cbe4344e16c4b8ca0a56692797b1
Emily Lu <emilylu840819@gmail.com>
2019-06-07 15:28:28 -0400

    send email to user after signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,16 @@ const bcrypt = require('bcryptjs');
 
 const User = require('../models/user');
 
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
+const sg_config = require('../config/sendgrid.js');
+
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: { api_key:sg_config.SG_KEY }
+  }));
+
+
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
@@ -69,6 +79,15 @@ exports.postSignup = (req, res, next) => {
         })
         .then(result => {
           res.redirect('/login');
+          return transporter.sendMail({
+            to: email,
+            from: 'customerservice@jeshop.com',
+            subject: 'Signup succeeded!',
+            html: '<h1>You successfully signed up!</h1>'
+          });
+        })
+        .catch(err => {
+          console.log(err);
         });
     })
     .catch(err => {
