aa7bff9104fe32595f821c12f706a2d3a31edc40
Emily Lu <emilylu840819@gmail.com>
2019-06-12 17:37:42 -0400

    Add recommend product section to index

--- controllers/shop.js
+++ controllers/shop.js
@@ -96,34 +96,27 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  const page = +req.query.page || 1;
-  let totalItems;
-  Product.find()
-  .countDocuments()
-  .then(numProducts => {
-    totalItems = numProducts;
-    return Product.find()
-      .skip((page - 1) * ITEMS_PER_PAGE)
-      .limit(ITEMS_PER_PAGE);
-  })
+
+  recommend((error, productData) => {
+    if (error) {
+      return console.log(error);
+  }  
+
+  Product.find( {_id: {$in: productData.body}} )
     .then(products => {
       res.render('shop/index', {
         user: req.user,
-        prods: products,
+        recprods: products,
         pageTitle: 'Shop',
         path: '/',
         isAuthenticated: req.session.isLoggedIn,       
-        currentPage: page,
-        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
-        hasPreviousPage: page > 1,
-        nextPage: page + 1,
-        previousPage: page - 1,
-        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
+  
       });
     })
     .catch(err => {
       console.log(err);
-    });
+    })
+  }) 
 };
 
 exports.getCart = (req, res, next) => {
